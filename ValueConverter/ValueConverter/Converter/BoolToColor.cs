9880444c4df17ca5b6c4233550f74a4fef8f8cd3
Gustavo-Luigi <gustavo_-_luigi@hotmail.com>
2021-01-25 20:57:43 -0300

    I Value Converter

---
+++ ValueConverter/ValueConverter/Converter/BoolToColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ValueConverter.Converter
+{
+    class BoolToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var booleanValue = (bool)value;
+
+            if (booleanValue)
+                return Brushes.Red;
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
