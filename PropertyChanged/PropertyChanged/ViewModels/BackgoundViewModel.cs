0ffa8ab27d04d63094e91473a243abf38146407a
Gustavo-Luigi <gustavo_-_luigi@hotmail.com>
2021-01-25 09:42:29 -0300

    I Notify Property Changed

---
+++ PropertyChanged/PropertyChanged/ViewModels/BackgoundViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PropertyChanged.ViewModels
+{
+    public class BackgoundViewModel : ObservableObject
+    {
+        private Brush _color;
+
+        public Brush Color
+        {
+            get
+            {
+                if (_color == null)
+                    return Brushes.Gray;
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("Color");
+            }
+        }
+    }
+}
---
+++ PropertyChanged/PropertyChanged/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PropertyChanged.ViewModels
+{
+    public class MainViewModel
+    {
+        public PersonViewModel Person { get; private set; }
+        public BackgoundViewModel Background { get; private set; }
+
+        public MainViewModel()
+        {
+            Person = new PersonViewModel();
+            Background = new BackgoundViewModel();
+        }
+
+        public void SetBackgorund(Brush brushColor)
+        {
+            Background.Color = brushColor;
+        }
+    }
+}
