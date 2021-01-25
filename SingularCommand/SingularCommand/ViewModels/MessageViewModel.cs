2d618021bc10adde33c6a14f193d8309e45e8bb8
Gustavo-Luigi <gustavo_-_luigi@hotmail.com>
2021-01-25 11:40:28 -0300

    ICommand - Singular Command

---
+++ SingularCommand/SingularCommand/ViewModels/MessageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SingularCommand.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingularCommand.ViewModels
+{
+    public class MessageViewModel
+    {
+        public MessageCommand DisplayMessageCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+
+        public void DisplayMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
